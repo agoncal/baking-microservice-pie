8df184594e65cebea8bed89e7ed494a8d439604c
Roberto Cortez <radcortez@yahoo.com>
2017-11-01 03:25:33 +0000

    Initial work for Demo 07 with Ribbon.

--- demo07-scale/services/book-api/src/main/java/org/bakingpie/book/rest/ConsulManagementService.java
+++ demo07-scale/services/book-api/src/main/java/org/bakingpie/book/rest/ConsulManagementService.java
@@ -18,10 +18,16 @@
 package org.bakingpie.book.rest;
 
 
+import com.netflix.client.ClientException;
+import com.netflix.client.config.DefaultClientConfigImpl;
+import com.netflix.loadbalancer.DynamicServerListLoadBalancer;
+import com.netflix.loadbalancer.Server;
+import com.netflix.loadbalancer.ServerList;
 import com.orbitz.consul.AgentClient;
 import com.orbitz.consul.Consul;
+import com.orbitz.consul.HealthClient;
 import com.orbitz.consul.model.agent.ImmutableRegistration;
-import com.orbitz.consul.model.agent.Registration;
+import com.orbitz.consul.model.health.ServiceHealth;
 import org.eclipse.microprofile.config.Config;
 import org.eclipse.microprofile.config.ConfigProvider;
 import org.slf4j.Logger;
@@ -31,14 +37,23 @@ import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
+import java.net.URI;
+import java.util.List;
 
+import static com.netflix.client.ClientFactory.registerNamedLoadBalancerFromclientConfig;
+import static com.netflix.client.config.CommonClientConfigKey.NFLoadBalancerClassName;
+import static com.netflix.client.config.CommonClientConfigKey.NFLoadBalancerRuleClassName;
+import static com.netflix.client.config.CommonClientConfigKey.ServerListRefreshInterval;
+import static com.netflix.client.config.DefaultClientConfigImpl.getClientConfigWithDefaultValues;
 import static com.orbitz.consul.model.agent.Registration.RegCheck.http;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
 
 @Singleton
 @Startup
 public class ConsulManagementService {
 
-    private final Logger log = LoggerFactory.getLogger(ConsulManagementService.class);
+    private static final Logger log = LoggerFactory.getLogger(ConsulManagementService.class);
 
     // Service
     private static final String BOOK_API_NAME = "CONSUL_BOOK_API";
@@ -51,7 +66,6 @@ public class ConsulManagementService {
 
     @PostConstruct
     protected void registerService() {
-
         final Config config = ConfigProvider.getConfig();
         config.getOptionalValue("BOOK_API_HOST", String.class).ifPresent(host -> bookApiHost = host);
         config.getOptionalValue("BOOK_API_PORT", Integer.class).ifPresent(port -> bookApiPort = port);
@@ -59,7 +73,7 @@ public class ConsulManagementService {
         config.getOptionalValue("CONSUL_PORT", Integer.class).ifPresent(port -> consulPort = port);
 
         log.info("Consul host and port " + consulHost + " " + consulPort);
-        Consul consul = Consul.builder().withUrl(consulHost + ":" + consulPort).build(); // connect to Consul on localhost
+        final Consul consul = Consul.builder().withUrl(consulHost + ":" + consulPort).build();
         agentClient = consul.agentClient();
 
         final ImmutableRegistration registration =
@@ -73,6 +87,8 @@ public class ConsulManagementService {
         agentClient.register(registration);
 
         log.info(BOOK_API_NAME + " is registered in consul on " + bookApiHost + ":" + bookApiPort);
+
+        registerLoadBalancer(consul);
     }
     @PreDestroy
     protected void unregisterService() {
@@ -80,5 +96,48 @@ public class ConsulManagementService {
         agentClient.deregister(BOOK_API_NAME);
 
         log.info(BOOK_API_NAME + " is un-registered from consul");
+    }
+
+    private void registerLoadBalancer(final Consul consul) {
+        try {
+            final DefaultClientConfigImpl clientConfig = getClientConfigWithDefaultValues("number-api");
+            clientConfig.set(NFLoadBalancerClassName, "com.netflix.loadbalancer.DynamicServerListLoadBalancer");
+            clientConfig.set(NFLoadBalancerRuleClassName, "com.netflix.loadbalancer.RoundRobinRule");
+            clientConfig.set(ServerListRefreshInterval, 10000);
+
+            final DynamicServerListLoadBalancer dynamicServerListLoadBalancer =
+                    (DynamicServerListLoadBalancer) registerNamedLoadBalancerFromclientConfig("number-api", clientConfig);
+            dynamicServerListLoadBalancer.setServerListImpl(new NumberApiServers(consul));
+            dynamicServerListLoadBalancer.enableAndInitLearnNewServersFeature();
+        } catch (final ClientException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static class NumberApiServers implements ServerList<Server> {
+        final Consul consul;
+
+        NumberApiServers(final Consul consul) {
+            this.consul = consul;
+        }
+
+        @Override
+        public List<Server> getInitialListOfServers() {
+            final HealthClient healthClient = consul.healthClient();
+            final List<ServiceHealth> nodes =
+                healthClient.getHealthyServiceInstances("CONSUL_NUMBER_API").getResponse();
+            final List<Server> servers = nodes.stream()
+                                              .map(serviceHealth -> new Server(
+                                                  URI.create(serviceHealth.getService().getAddress()).getHost(),
+                                                  serviceHealth.getService().getPort()))
+                                              .collect(toList());
+            log.info("Updating Load Balancers servers: " + servers.stream().map(Server::getId).collect(joining(",")));
+            return servers;
+        }
+
+        @Override
+        public List<Server> getUpdatedListOfServers() {
+            return getInitialListOfServers();
+        }
     }
 }
