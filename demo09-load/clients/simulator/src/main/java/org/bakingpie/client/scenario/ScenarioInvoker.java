47a41c0fee26c5c046a36191074cb2e313570db4
Roberto Cortez <radcortez@yahoo.com>
2017-11-02 01:53:35 +0000

    Added Client Simulator to Demo 09.

---
+++ demo09-load/clients/simulator/src/main/java/org/bakingpie/client/scenario/ScenarioInvoker.java
@@ -0,0 +1,84 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.bakingpie.client.scenario;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.inject.Inject;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Response;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
+
+import static java.lang.String.format;
+
+public abstract class ScenarioInvoker implements Runnable {
+    private static Logger logger = Logger.getLogger(ScenarioInvoker.class.getName());
+
+    @Inject
+    private Client client;
+
+    private boolean execute;
+    private WeightedRandomResult<Endpoint> endpointsToExecute;
+
+    protected abstract String getTargetUrl();
+
+    protected abstract List<Endpoint> getEndpoints();
+
+    @PostConstruct
+    private void init() {
+        getEndpoints();
+        this.execute = true;
+        this.endpointsToExecute = distributeEndpoints();
+    }
+
+    protected WeightedRandomResult<Endpoint> distributeEndpoints() {
+        return new WeightedRandomResult<>(getEndpoints());
+    }
+
+    @Override
+    public void run() {
+        while (execute) {
+            try {
+                final Endpoint endpoint = endpointsToExecute.get();
+                final WebTarget webTarget = client.target(getTargetUrl())
+                                                  .path(endpoint.getPath())
+                                                  .resolveTemplates(endpoint.getTemplates());
+                final Response response = webTarget.request().method(endpoint.getMethod(), endpoint.getEntity());
+                logger.info(format("%s - %s - %d", endpoint.getMethod(), webTarget.getUri(), response.getStatus()));
+                sleep();
+            } catch (final Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void sleep() {
+        try {
+            TimeUnit.MILLISECONDS.sleep(500); // TODO - Externalize
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @PreDestroy
+    private void destroy() {
+        this.execute = false;
+    }
+}
---
+++ demo09-load/clients/simulator/src/main/java/org/bakingpie/client/scenario/book/BookScenario.java
@@ -0,0 +1,103 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.bakingpie.client.scenario.book;
+
+import com.github.javafaker.Faker;
+import org.bakingpie.client.auth.BasicAuth;
+import org.bakingpie.client.scenario.Endpoint;
+import org.bakingpie.client.scenario.ScenarioInvoker;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.GenericType;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.collectingAndThen;
+import static java.util.stream.Collectors.toList;
+import static javax.ws.rs.client.Entity.json;
+import static javax.ws.rs.client.Entity.text;
+import static org.bakingpie.client.scenario.Endpoint.endpoint;
+import static org.bakingpie.client.scenario.Endpoint.endpointWithEntity;
+import static org.bakingpie.client.scenario.Endpoint.endpointWithTemplates;
+
+@ApplicationScoped
+@BasicAuth
+public class BookScenario extends ScenarioInvoker {
+    @Inject
+    private Faker faker;
+
+    @Inject
+    @ConfigProperty(name = "BOOK_TARGET_URL", defaultValue = "http://localhost:8081")
+    private String targetUrl;
+
+    @Override
+    protected String getTargetUrl() {
+        return targetUrl;
+    }
+
+    @Override
+    protected List<Endpoint> getEndpoints() {
+        final String contextRoot = "book-api/api"; // TODO - Externalize
+        return Stream.of(endpoint(contextRoot + "/books", "GET"),
+                         endpointWithEntity(contextRoot + "/books", "POST", this::createBook),
+                         endpointWithTemplates(contextRoot + "/books/{id}", "DELETE", this::deleteBook),
+                         endpointWithTemplates(contextRoot + "/books/{id}", "GET", this::findBook))
+                     .collect(collectingAndThen(toList(), Collections::unmodifiableList));
+    }
+
+    private Entity createBook() {
+        if (Math.random() * 100 < 95) {
+            final com.github.javafaker.Book book = faker.book();
+            return json(new Book(book.author(),
+                                 book.title(),
+                                 faker.number().numberBetween(1900, 2017),
+                                 book.genre(),
+                                 faker.number().digits(13)));
+        } else {
+            return text("");
+        }
+    }
+
+    private Map<String, Object> deleteBook() {
+        final HashMap<String, Object> templates = new HashMap<>();
+        templates.put("id", getRandomBook());
+        return templates;
+    }
+
+    private Map<String, Object> findBook() {
+        final HashMap<String, Object> templates = new HashMap<>();
+        templates.put("id", getRandomBook());
+        return templates;
+    }
+
+    @Inject
+    private Client client;
+
+    private Integer getRandomBook() {
+        final WebTarget webTarget = client.target(targetUrl).path("book-api/api/books");
+        final List<Book> books = webTarget.request().get(new GenericType<List<Book>>() {});
+        return books.isEmpty() ? 1 : books.get((int) (Math.random() * books.size())).getId();
+    }
+}
