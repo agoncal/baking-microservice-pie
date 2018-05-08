a9868f67896987df0e7578a74b9a21b584a8c7a3
Antonio Goncalves <antonio.goncalves@gmail.com>
2018-05-08 17:56:01 +0100

    Add Consul to simulators

--- demo09-load/clients/simulator/src/main/java/org/bakingpie/client/Initializer.java
+++ demo09-load/clients/simulator/src/main/java/org/bakingpie/client/Initializer.java
@@ -16,7 +16,12 @@
  */
 package org.bakingpie.client;
 
+import com.orbitz.consul.AgentClient;
+import com.orbitz.consul.Consul;
+import com.orbitz.consul.model.agent.ImmutableRegistration;
 import org.bakingpie.client.scenario.ScenarioInvoker;
+import org.eclipse.microprofile.config.Config;
+import org.eclipse.microprofile.config.ConfigProvider;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
@@ -27,11 +32,28 @@ import javax.ejb.Startup;
 import javax.enterprise.concurrent.ManagedExecutorService;
 import javax.enterprise.inject.Instance;
 import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+import java.util.UUID;
+
+import static com.orbitz.consul.model.agent.Registration.RegCheck.http;
 
 @Singleton
 @Startup
 @Lock(LockType.READ)
+@Path("health")
 public class Initializer {
+
+    // Service
+    private static final String SIMULATOR_NAME = "CONSUL_SIMULATOR";
+    private String simulatorHost = "http://localhost";
+    private Integer simulatorPort = 8080;
+    // Consul
+    private String consulHost = "http://localhost";
+    private Integer consulPort = 8500;
+    private AgentClient agentClient;
+
     @Resource
     private ManagedExecutorService mes;
 
@@ -40,6 +62,34 @@ public class Initializer {
 
     @PostConstruct
     void postConstruct() {
+        registerService();
         scenarioInvokers.forEach(mes::execute);
+    }
+
+    protected void registerService() {
+
+        final Config config = ConfigProvider.getConfig();
+        config.getOptionalValue("SIMULATOR_HOST", String.class).ifPresent(host -> simulatorHost = host);
+        config.getOptionalValue("SIMULATOR_PORT", Integer.class).ifPresent(port -> simulatorPort = port);
+        config.getOptionalValue("CONSUL_HOST", String.class).ifPresent(host -> consulHost = host);
+        config.getOptionalValue("CONSUL_PORT", Integer.class).ifPresent(port -> consulPort = port);
+
+        final Consul consul = Consul.builder().withUrl(consulHost + ":" + consulPort).build();
+        agentClient = consul.agentClient();
+
+        final ImmutableRegistration registration =
+                ImmutableRegistration.builder()
+                        .id(UUID.randomUUID().toString())
+                        .name(SIMULATOR_NAME)
+                        .address(simulatorHost)
+                        .port(simulatorPort)
+                        .check(http(simulatorHost + ":" + simulatorPort + "/simulator/api/health", 5))
+                        .build();
+        agentClient.register(registration);
+        }
+
+    @GET
+    public Response health() {
+        return Response.ok().build();
     }
 }
