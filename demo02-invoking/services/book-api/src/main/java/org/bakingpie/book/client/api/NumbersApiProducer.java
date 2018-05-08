7d8167ce3332b790f52303c3104a029b3aac7122
Antonio Goncalves <antonio.goncalves@gmail.com>
2018-05-08 16:20:11 +0100

    adding asciidoc snippets

--- demo02-invoking/services/book-api/src/main/java/org/bakingpie/book/client/api/NumbersApiProducer.java
+++ demo02-invoking/services/book-api/src/main/java/org/bakingpie/book/client/api/NumbersApiProducer.java
@@ -5,10 +5,13 @@ import org.bakingpie.book.client.ApiClient;
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Produces;
 
+// tag::adocSnippet[]
 @ApplicationScoped
 public class NumbersApiProducer {
+
     @Produces
     public NumbersApi createNumbersApi() {
         return new ApiClient().buildNumberApiClient();
     }
 }
+// end::adocSnippet[]
